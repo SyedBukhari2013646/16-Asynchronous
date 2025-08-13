c0d00946d40b17fa3bb71259a06ed2a297a5cee2
SyedBukhari2013646 <syedbukhari03249334041@gmail.com>
2025-08-13 22:15:47 +0500

    ki

--- starter/script.js
+++ starter/script.js
@@ -10,3 +10,115 @@ const countriesContainer = document.querySelector('.countries');
 // https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
 
 ///////////////////////////////////////
+// const getCountryData = function (country) {
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+//   request.send();
+
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
+
+//     const html = ` <article class="country">
+//           <img class="country__img" src="${data.flags.png}" />
+//           <div class="country__data">
+//             <h3 class="country__name">${data.name.common}</h3>
+//             <h4 class="country__region">${data.region}</h4>
+//             <p class="country__row"><span>👫</span>${(
+//               +data.population / 10000000
+//             ).toFixed(1)}</p>
+//             <p class="country__row"><span>🗣️</span>${Object.values(
+//               data.languages
+//             ).join(', ')}</p>
+//             <p class="country__row"><span>💰</span>${Object.values(
+//               data.currencies
+//             )
+//               .map(curr => curr.name)
+//               .join(', ')}</p>
+//           </div>
+//         </article>`;
+
+//     countriesContainer.insertAdjacentHTML('beforeend', html);
+//     countriesContainer.style.opacity = 1;
+//   });
+// };
+
+// getCountryData('pakistan');
+// getCountryData('bharat');
+// getCountryData('china');
+// getCountryData('iran');
+// getCountryData('afghanistan');
+
+///////////////////////////////////////////
+// Render country
+// const renderCountry = function (data, countryName = '') {
+//   const html = ` <article class="country ${countryName}">
+// <img class="country__img" src="${data.flags.png}" />
+// <div class="country__data">
+//   <h3 class="country__name">${data.name.common}</h3>
+//   <h4 class="country__region">${data.region}</h4>
+//   <p class="country__row"><span>👫</span>${(
+//     +data.population / 10000000
+//   ).toFixed(1)}</p>
+//   <p class="country__row"><span>🗣️</span>${Object.values(data.languages).join(
+//     ', '
+//   )}</p>
+//   <p class="country__row"><span>💰</span>${Object.values(data.currencies)
+//     .map(curr => curr.name)
+//     .join(', ')}</p>
+// </div>
+// </article>`;
+
+//   countriesContainer.insertAdjacentHTML('beforeend', html);
+//   countriesContainer.style.opacity = 1;
+// };
+
+////////////////////////////////////////////
+// Get neighbouring countries
+
+// AJAX country call 1
+// const getCountryNeighbour = function (country) {
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+//   request.send();
+
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
+
+//     // Render country 1
+//     renderCountry(data);
+
+//     // Get neighbour country 2
+//     const [neighbour] = data.borders;
+//     if (!neighbour) return;
+
+//     const request2 = new XMLHttpRequest();
+//     request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+//     request2.send();
+
+//     request2.addEventListener('load', function () {
+//       const [data2] = JSON.parse(this.responseText);
+//       console.log(data2);
+
+//       renderCountry(data2, 'neighbour');
+//     });
+//   });
+// };
+
+// getCountryNeighbour('pakistan');
+// getCountryNeighbour('bharat');
+// getCountryNeighbour('china');
+// getCountryNeighbour('iran');
+// getCountryNeighbour('russia');
+
+///////////////////////////////////////////
+// Callback hell and promises
+
+// Before it was like this
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+//   request.send();
+
+const request = fetch('https://restcountries.com/v3.1/name/pakistan');
+console.log(request);
