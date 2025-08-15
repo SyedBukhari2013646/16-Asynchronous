51c9741c40c4923d2e5941c40b18af10f984031c
SyedBukhari2013646 <syedbukhari03249334041@gmail.com>
2025-08-15 21:34:11 +0500

    hi

--- starter/script.js
+++ starter/script.js
@@ -3,6 +3,36 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+///////////////////////////////////////////
+// Render country
+const renderCountry = function (data, countryName = '') {
+  const html = ` <article class="country ${countryName}">
+<img class="country__img" src="${data.flags.png}" />
+<div class="country__data">
+  <h3 class="country__name">${data.name.common}</h3>
+  <h4 class="country__region">${data.region}</h4>
+  <p class="country__row"><span>👫</span>${(
+    +data.population / 10000000
+  ).toFixed(1)}</p>
+  <p class="country__row"><span>🗣️</span>${Object.values(data.languages).join(
+    ', '
+  )}</p>
+  <p class="country__row"><span>💰</span>${Object.values(data.currencies)
+    .map(curr => curr.name)
+    .join(', ')}</p>
+</div>
+</article>`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+//////////////////////////////////////////
+// Render Error
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  countriesContainer.style.opacity = 1;
+};
 // NEW COUNTRIES API URL (use instead of the URL shown in videos):
 // https://restcountries.com/v2/name/portugal
 
@@ -48,30 +78,6 @@ const countriesContainer = document.querySelector('.countries');
 // getCountryData('china');
 // getCountryData('iran');
 // getCountryData('afghanistan');
-
-///////////////////////////////////////////
-// Render country
-const renderCountry = function (data, countryName = '') {
-  const html = ` <article class="country ${countryName}">
-<img class="country__img" src="${data.flags.png}" />
-<div class="country__data">
-  <h3 class="country__name">${data.name.common}</h3>
-  <h4 class="country__region">${data.region}</h4>
-  <p class="country__row"><span>👫</span>${(
-    +data.population / 10000000
-  ).toFixed(1)}</p>
-  <p class="country__row"><span>🗣️</span>${Object.values(data.languages).join(
-    ', '
-  )}</p>
-  <p class="country__row"><span>💰</span>${Object.values(data.currencies)
-    .map(curr => curr.name)
-    .join(', ')}</p>
-</div>
-</article>`;
-
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
-};
 
 ////////////////////////////////////////////
 // Get neighbouring countries
@@ -120,8 +126,8 @@ const renderCountry = function (data, countryName = '') {
 //   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
 //   request.send();
 
-const request = fetch('https://restcountries.com/v3.1/name/pakistan');
-console.log(request);
+// const request = fetch('https://restcountries.com/v3.1/name/pakistan');
+// console.log(request);
 
 // const getCountryData = function (country) {
 //   fetch(`https://restcountries.com/v3.1/name/${country}`)
@@ -137,21 +143,88 @@ console.log(request);
 
 //////////////////////////////////////////////
 // Promises look a lot more cleaner and beautifull then the other
+
+// const getJSON = function (url, errorMsg = 'Somethibg went wrong') {
+//   return fetch(url).then(response => {
+//     if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+
+//     return response.json();
+//   });
+// };
+// const getCountryData = function (country) {
+//   // Country 1
+//   fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     .then(response => {
+//       console.log(response);
+
+//       if (!response.ok)
+//         throw new Error(`Country not found (${response.status})`);
+
+//       return response.json();
+//     })
+//     .then(data => {
+//       // Show main country
+//       renderCountry(data[0]);
+
+//       // Get neighbours
+//       const neighbours = data[0].borders;
+//       if (!neighbours) return;
+
+//       // Show each neighbour
+//       neighbours.forEach(code => {
+//         fetch(`https://restcountries.com/v3.1/alpha?codes=${code}`)
+//           .then(response => response.json())
+//           .then(data => renderCountry(data[0], 'neighbour'));
+//       });
+//     })
+//     .catch(err => {
+//       console.log(`${err} 💥💥💥`);
+//       renderError(`Something went wrong 💥💥 ${err.message}. Try again!`);
+//     })
+//     .finally(() => {
+//       countriesContainer.style.opacity = 1;
+//     });
+// };
+
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+    return response.json();
+  });
+};
+
 const getCountryData = function (country) {
-  // Country 1
-  fetch(`https://restcountries.com/v3.1/name/${country}`)
-    .then(response => response.json())
+  getJSON(`https://restcountries.com/v3.1/name/${country}`, 'Country not found')
     .then(data => {
-      renderCountry(data[0], 'neighbour');
-      const neighbour = data[0].borders;
+      // Show main country
+      renderCountry(data[0]);
 
-      if (!neighbour) return;
+      // Get neighbours
+      const neighbours = data[0].borders;
+      if (!neighbours) throw new Error('No neighbour found!');
 
-      return neighbour.forEach(code => {
-        fetch(`https://restcountries.com/v3.1/alpha?codes=${code}`)
-          .then(responce => responce.json())
-          .then(data => renderCountry(data[0], 'neighbour'));
-      });
+      // Return fetch Promise for neighbours
+      return getJSON(
+        `https://restcountries.com/v3.1/alpha?codes=${neighbours.join(',')}`,
+        'Country not found'
+      );
+    })
+    .then(data => {
+      // Render neighbours if any
+      if (!data) return;
+      data.forEach(country => renderCountry(country, 'neighbour'));
+    })
+    .catch(err => {
+      console.error(`${err.message} 💥`);
+      renderError(`Something went wrong 💥 ${err.message}. Try again!`);
+    })
+    .finally(() => {
+      // Always run at the end
+      countriesContainer.style.opacity = 1;
     });
 };
-getCountryData('bharat');
+
+// Attach listener once
+btn.addEventListener('click', function () {
+  getCountryData('australia');
+});
